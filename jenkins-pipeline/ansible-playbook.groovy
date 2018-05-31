dfa50dfb748bc4785c424d564dde25ba8953252e
Serhiy Makarenko <serhiy@makarenko.me>
2018-05-31 10:25:53 +0300

    Change stage name
    Change stage name

--- jenkins-pipeline/ansible-playbook.groovy
+++ jenkins-pipeline/ansible-playbook.groovy
@@ -2,7 +2,7 @@
 
 def ansibleVmProvisionSyntaxValidate() {
 	git branch: "${BRANCH}", credentialsId: '347a97e7-6224-45f3-84ac-4c9c4709121c', url: "${ANSIBLE_URL}"
-	stage('Playbook validate') {
+	stage('Playbook syntax check') {
 		sh 'chmod 400 .ssh/id_rsa'
 		sh "ansible-playbook --syntax-check -i hosts task.yml "
 	}
